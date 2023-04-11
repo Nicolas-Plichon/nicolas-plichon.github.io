37a00d33d112248df0702e2fe83cc39f2b7c3fd5
Nicolas Plichon <nicolas.plichon@gmail.com>
2023-04-11 06:26:16 +0200

    Ajout du menu de navigation + animation

--- js/app.js
+++ js/app.js
@@ -20,3 +20,39 @@ new Typewriter(txtAnim, {
 .changeDeleteSpeed(20)
 .deleteChars(9)
 .start()
+
+// Changer le menu au scroll
+function cleanClasses() {
+  const navActive = document.querySelector('.nav-active');
+  navActive.classList.add('nav-inactive');
+  navActive.classList.remove('nav-active');
+}
+
+const navHome = document.getElementById('buttonHome');
+const navAbout = document.getElementById('buttonAbout');
+const navSkills = document.getElementById('buttonSkills');
+const navProjects = document.getElementById('buttonProjects');
+const navContact = document.getElementById('buttonContact');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY < 979) {
+    cleanClasses();
+    navHome.classList.remove('nav-inactive');
+    navHome.classList.add('nav-active');
+  } else if (window.scrollY >= 3916) {
+    cleanClasses();
+    navContact.classList.remove('nav-inactive');
+    navContact.classList.add('nav-active');
+  } else if (window.scrollY >= 2937) {
+    cleanClasses();
+    navProjects.classList.remove('nav-inactive');
+    navProjects.classList.add('nav-active');
+  } else if (window.scrollY >= 1958) {
+    cleanClasses();
+    navSkills.classList.remove('nav-inactive');
+    navSkills.classList.add('nav-active');
+  } else if (window.scrollY >= 979) {
+    cleanClasses();
+    navAbout.classList.remove('nav-inactive');
+    navAbout.classList.add('nav-active');
+  }});
